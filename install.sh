e7d2a550e9ef1094c96f8ac7680b372047e280a2
Tali Herzka <therzka@github.com>
2021-11-04 16:34:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
 rm -f $HOME/.zshrc
 ln -s $(pwd)/zshrc $HOME/.zshrc
+
+git clone https://github.com/Flower7C3/oh-flowers-zsh-theme.git $HOME/.oh-my-zsh/custom/themes/flower7c3
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
