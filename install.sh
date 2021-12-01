56e3255a878f1261b03c63b2bd00dbe5dcc35205
Tali Herzka <therzka@github.com>
2021-11-30 17:34:38 -0800

    simplify

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#!/bin/sh
-
-[ "${SHELL##/*/}" != "zsh" ] && echo 'You might need to change default shell to zsh: `chsh -s /bin/zsh`'
-
-dir="$HOME/Developer/personal"
-mkdir -p $dir
-cd $dir
-
-git clone --recursive https://github.com/therzka/dotfiles.git
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-cd dotfiles
-sh etc/symlink-dotfiles.sh
+cat zshrc > $HOME/.zshrc
+source ~/.zshrc
